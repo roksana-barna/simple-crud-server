f8aa4648d6b17c90fb73f63a19d5361ebf4f9347
roksana-barna <roksanabarna1803@gmail.com>
2023-05-10 02:02:16 +0600

    update

--- index.js
+++ index.js
@@ -32,8 +32,33 @@ async function run() {
         const result =await cursor.toArray();
         res.send(result);
     })
+// update
+    app.get('/users/:id',async(req,res)=>{
+      const id =req.params.id;
+      const query={_id:new ObjectId(id)}
+      const user = await userCollection.findOne(query)
+      res.send(user);
+    });
+
+// put
+app.put('/users/:id',async(req,res)=>{
+  const id =req.params.id;
+  const user =req.body;
+  console.log(user);
+  const filter ={_id:new ObjectId(id)}
+  const options ={upsert:true}
+  const updatedUser ={
+    $set:{
+     name:user.name,
+     email:user.email 
+    }
+
+  }
+  const result = await userCollection.updateOne(filter,updatedUser,options);
+  res.send(result)
 
 
+})
     app.post('/users',async(req,res)=>{
         const user =req.body;
         console.log('new user',user)
@@ -43,7 +68,7 @@ async function run() {
 
     });
     // for delete
-    app.delete('users/:id',async(req,res) =>{
+    app.delete('/users/:id',async(req,res) =>{
         const id = req.params.id;
         console.log(id)
         const query = { _id: new ObjectId(id)}
